f7e4fa1c3654795e8db67a3cca4ecec02c7b17be
Max Nikitin <40297478+ITSalt@users.noreply.github.com>
2023-05-13 13:09:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -25,25 +25,42 @@ function splitText() {
       blocks.push(currentBlock);
     }
 
+    if (blocks.length > 0) {
+      var introBlock = document.createElement("div");
+      introBlock.classList.add("block");
+
+      var introHeader = document.createElement("div");
+      introHeader.classList.add("block-header");
+      var introHeaderText = document.createElement("div");
+      introHeaderText.classList.add("block-header-text");
+      introHeaderText.innerText = "Мой текст превышает ограничение по количеству символов, которое можно вставить в одно сообщение, поэтому я разделю его на " + blocks.length + " сообщений. Каждое следующее сообщение будет частью общего текста, в начале будет указан номер части.
+      introHeader.appendChild(introHeaderText);
+      introBlock.appendChild(introHeader);
+
+      output.appendChild(introBlock);
+    }
+
     for (var j = 0; j < blocks.length; j++) {
       var block = document.createElement("div");
       block.classList.add("block");
 
       var header = document.createElement("div");
       header.classList.add("block-header");
-      header.innerText = "Вот часть номер " + (blockIndex++);
-      block.appendChild(header);
-
-      var content = document.createElement("div");
-      content.innerText = blocks[j];
-      block.appendChild(content);
-
+      var headerText = document.createElement("div");
+      headerText.classList.add("block-header-text");
+      headerText.innerText = "Часть номер " + (j + 1);
+      header.appendChild(headerText);
       var copyButton = document.createElement("button");
       copyButton.innerText = "Скопировать в буфер обмена";
       copyButton.onclick = function() {
         copyToClipboard(blocks[j]);
       };
-      block.appendChild(copyButton);
+      header.appendChild(copyButton);
+      block.appendChild(header);
+
+      var content = document.createElement("div");
+      content.innerText = blocks[j];
+      block.appendChild(content);
 
       output.appendChild(block);
     }
@@ -55,7 +72,7 @@ function splitText() {
 
 function copyToClipboard(text) {
   var textarea = document.createElement("textarea");
-    textarea.value = text;
+  textarea.value = text;
   document.body.appendChild(textarea);
   textarea.select();
   document.execCommand("copy");
